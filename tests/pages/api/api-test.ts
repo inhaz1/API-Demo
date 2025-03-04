93f814e3de18cf00731de624249212fc71bbc4a4
inhaz1 <izaheen@anaconda.com>
2025-03-04 15:44:24 +0500

    Added dummy API tests

--- tests/pages/api/api-test.ts
+++ tests/pages/api/api-test.ts
@@ -2,22 +2,24 @@ import { expect, getRequest, postRequest } from '@anaconda/playwright-utils';
 import { validPostReqData } from '@testdata/user-data';
 
 export class APITest {
-  private readonly UserListRequest = `https://jsonplaceholder.typicode.com/posts/1`;
-  private readonly PostRequest = `https://jsonplaceholder.typicode.com/posts`;
+  private readonly userListRequest = `https://jsonplaceholder.typicode.com/posts/1`;
+  private readonly userPostRequest = `https://jsonplaceholder.typicode.com/posts`;
 
   async verifyGetUserListRequestWithSuccessResponse(): Promise<void> {
-    const response = await getRequest(this.UserListRequest);
+    const response = await getRequest(this.userListRequest);
     console.log(response.status());
     console.log(response.statusText());
 
-    expect(response.status(), `status should be 200`).toBe(200);
+    expect(response.status(), `status should be 200`).toBe(400);
     expect(response.statusText(), `status text should be ok`).toBe('OK');
     const responseBody = await response.json();
     console.log(responseBody);
+    //expect(responseBody.userId, `userId should be 1`).toBe(1);
+    //expect.soft(responseBody.id, `id should be 1`).toBe(1);
   }
 
   async verifyCreateUserPostRequestWithSuccessResponse(): Promise<void> {
-    const response = await postRequest(this.PostRequest, {
+    const response = await postRequest(this.userPostRequest, {
       data: validPostReqData,
     });
     expect(response.status(), `status should be 201`).toBe(201);
